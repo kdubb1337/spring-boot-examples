60a56aef51606f2a2720bc6a69c4b3c3cda680ae
Erich Reich <erich.reich@gmail.com>
2014-02-23 22:09:23 -0500

    Updated Application ordering

--- simple/src/main/java/com/kdubb/springboot/Application.java
+++ simple/src/main/java/com/kdubb/springboot/Application.java
@@ -11,14 +11,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
 	@Bean
 	public InternalResourceViewResolver setupViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
-	} 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
 	}
 }
