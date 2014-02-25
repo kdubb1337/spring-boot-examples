9fa498cbd73ed1281507ab19af8056649df13ee9
Erich Reich <erich.reich@gmail.com>
2014-02-24 20:21:59 -0500

    Added simple and deployable war projects for boot

--- deployable-war/src/main/java/com/kdubb/springboot/Application.java
+++ deployable-war/src/main/java/com/kdubb/springboot/Application.java
@@ -2,6 +2,8 @@ package com.kdubb.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,14 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 	
 	@Bean
