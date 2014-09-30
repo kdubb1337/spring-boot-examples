d72b59e5b2c3d7211fbbb39af407b99f13cc6abd
Erich Reich <erich.reich@gmail.com>
2014-09-30 17:22:35 -0400

    Updated to use Java 8 and Tomcat 8

--- deployable-war/src/main/java/com/kdubb/springboot/HelloController.java
+++ deployable-war/src/main/java/com/kdubb/springboot/HelloController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
-    @RequestMapping(value="/", method=RequestMethod.GET)
-    public String index(Model model) {
-   	 // let’s pass some variables to the view script
-   	 model.addAttribute("wisdom", "Goodbye XML");
-	 		
-   	 // renders /WEB-INF/views/hello.jsp
-   	 return "hello"; 
-    }
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		// letï¿½s pass some variables to the view script
+		model.addAttribute("wisdom", "Goodbye XML");
+
+		// renders /WEB-INF/views/hello.jsp
+		return "hello";
+	}
 }
