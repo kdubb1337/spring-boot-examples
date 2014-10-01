8429fc15fbf663b9f2250b3e69245b07681e00de
Erich Reich <erich.reich@gmail.com>
2014-10-01 10:16:04 -0400

    Added Java 8 Test

--- deployable-war/src/main/java/com/kdubb/springboot/HelloController.java
+++ deployable-war/src/main/java/com/kdubb/springboot/HelloController.java
@@ -1,5 +1,7 @@
 package com.kdubb.springboot;
 
+import java.time.Instant;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
+		Instant now = Instant.now();
+		
 		// let�s pass some variables to the view script
-		model.addAttribute("wisdom", "Goodbye XML");
-
+		model.addAttribute("wisdom", "Java 8 [" + now + "]");
+		
 		// renders /WEB-INF/views/hello.jsp
 		return "hello";
 	}
