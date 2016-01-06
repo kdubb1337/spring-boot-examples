d6ac5ff4d8011a05c4d1910a4b602fa5b1bd3863
Erich Reich <ereich@eyereturn.com>
2016-01-06 12:24:07 -0500

    Updated with java8, banner, logback, and YML file

--- simple/src/main/java/com/kdubb/springboot/Application.java
+++ simple/src/main/java/com/kdubb/springboot/Application.java
@@ -1,25 +1,29 @@
 package com.kdubb.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.core.env.AbstractEnvironment;
 
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
 public class Application {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
+		init();
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Bean
-	public InternalResourceViewResolver setupViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		return resolver;
+
+	private static void init() {
+		String defaultEnv = System.getProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME);
+		String env = System.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, defaultEnv);
+
+		LOG.warn("Starting with environment -> {" + env + "}");
 	}
 }
